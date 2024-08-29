8462030d9a303069dcae2cce7e01cd90aefb923a
CECILIA-MULANDI <mulandicecilia4@gmail.com>
2024-08-29 22:31:55 +0300

    Remove target directory from repository

--- target/debug/check_if_number_is_prime.d
+++
@@ -1,1 +0,0 @@
-/home/cecilia/Personal_Implementations_Of_MathWeb3Cohort/check_if_number_is_prime/target/debug/check_if_number_is_prime: /home/cecilia/Personal_Implementations_Of_MathWeb3Cohort/check_if_number_is_prime/src/main.rs
--- target/debug/deps/check_if_number_is_prime-2fbc4d2387ef82e9.d
+++
@@ -1,5 +0,0 @@
-/home/cecilia/Personal_Implementations_Of_MathWeb3Cohort/check_if_number_is_prime/target/debug/deps/libcheck_if_number_is_prime-2fbc4d2387ef82e9.rmeta: src/main.rs
-
-/home/cecilia/Personal_Implementations_Of_MathWeb3Cohort/check_if_number_is_prime/target/debug/deps/check_if_number_is_prime-2fbc4d2387ef82e9.d: src/main.rs
-
-src/main.rs:
